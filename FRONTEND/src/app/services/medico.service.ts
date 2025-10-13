50f18e69e90957f13cacd3dcd11e4ce7bbffc399
Nahuel Mangano <mangano.nahuel@gmail.com>
2025-10-13 00:05:06 -0300

    agrego medico.service.ts

--- FRONTEND/src/app/services/medico.service.ts
+++ FRONTEND/src/app/services/medico.service.ts
@@ -1,8 +1,36 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { API_URL } from '../app.config'; // token de configuración
+import { ResponseApi } from '../interfaces/response-api';
+import { Medico } from '../interfaces/medico';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MedicoService {
+    private http = inject(HttpClient);
+    private apiUrl = inject(API_URL);
+
+    private urlApi = this.apiUrl + 'Medico/';
+
+    lista(): Observable<ResponseApi> {
+        return this.http.get<ResponseApi>(this.urlApi + 'Lista');
+    }
+
+    editar(request: Medico): Observable<ResponseApi> {
+        return this.http.put<ResponseApi>(this.urlApi + 'Editar', request);
+    } 
+
+    eliminar(id: number): Observable<ResponseApi> {
+        return this.http.delete<ResponseApi>(this.urlApi + 'Eliminar/' + id);
+    }
+
+
   
 }
+
+
+
+
+
