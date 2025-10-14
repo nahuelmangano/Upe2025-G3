484b0e4e2d9549819dc4a2bc3db820fb1c8b7a1b
Manuel N. Mediavilla Grosjean <mediavilla.grosjean@gmail.com>
2025-10-14 04:51:53 -0300

    Agregar campo-valor service

--- FRONTEND/src/app/services/campo-valor.service.ts
+++ FRONTEND/src/app/services/campo-valor.service.ts
@@ -1,8 +1,31 @@
-import { Injectable } from '@angular/core';
+import { Injectable, inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { API_URL } from '../app.config'; // token de configuración
+import { ResponseApi } from '../interfaces/response-api';
+import { CampoValor } from '../interfaces/campoValor';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CampoValorService {
-  
+
+  private http = inject(HttpClient);
+  private apiUrl = inject(API_URL);
+
+  private urlApi = this.apiUrl + 'CampoValor/';
+
+  listaPorCampoEvolucion(campoId: number, evolucionId: number): Observable<ResponseApi> {
+    const url = `${this.urlApi}ListaPorCampoEvolucion/${campoId}/${evolucionId}`;
+    return this.http.get<ResponseApi>(url);
+  }
+
+  crear(request: CampoValor): Observable<ResponseApi> {
+    return this.http.post<ResponseApi>(`${this.urlApi}Crear`, request);
+  }
+
+  editar(request: CampoValor): Observable<ResponseApi> {
+    return this.http.put<ResponseApi>(`${this.urlApi}Editar`, request);
+  }
+
 }
