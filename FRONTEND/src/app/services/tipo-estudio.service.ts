d0e7d5511a391171aa34c7edf265eef5d233152b
ch-unnamed <pepe.n.acuna@gmail.com>
2025-10-15 15:28:56 -0300

    Add Service (rol-permiso,permiso,tipo-estudio,especialidad)

--- FRONTEND/src/app/services/tipo-estudio.service.ts
+++ FRONTEND/src/app/services/tipo-estudio.service.ts
@@ -1,8 +1,27 @@
-import { Injectable } from '@angular/core';
-
+import { Injectable, inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { API_URL } from '../app.config'; // token de configuración
+import { ResponseApi } from '../interfaces/response-api';
+import { TipoEstudio } from '../interfaces/tipo-estudio';
 @Injectable({
   providedIn: 'root'
 })
 export class TipoEstudioService {
-  
+  private http = inject(HttpClient);
+  private apiUrl = inject(API_URL);
+  private urlApi = this.apiUrl + 'TipoEstudio/';
+
+    lista(): Observable<ResponseApi> {
+    return this.http.get<ResponseApi>(`${this.urlApi}Lista`);
+    }
+    crear(request: TipoEstudio): Observable<ResponseApi> {
+    return this.http.post<ResponseApi>(`${this.urlApi}Crear`, request);
+    }
+    editar(request: TipoEstudio): Observable<ResponseApi> {
+    return this.http.put<ResponseApi>(`${this.urlApi}Editar`, request);
+    }
+    eliminar(id: number): Observable<ResponseApi> {
+    return this.http.put<ResponseApi>(`${this.urlApi}Eliminar/${id}`, {});
+    }
 }
