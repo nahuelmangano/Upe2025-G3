074f179dea8b9c7a7ef07aebc732e0f9df116d5e
CubinaMartin <131369620+CubinaMartin@users.noreply.github.com>
2025-10-15 16:16:52 -0300

    estudio, plan salud y plantilla service

--- FRONTEND/src/app/services/estudio.service.ts
+++ FRONTEND/src/app/services/estudio.service.ts
@@ -1,8 +1,28 @@
-import { Injectable } from '@angular/core';
+import { Injectable, inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { API_URL } from '../app.config';
+import { ResponseApi } from '../interfaces/response-api';
+import { Estudio } from '../interfaces/estudio';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EstudioService {
-  
+  private http = inject(HttpClient);
+  private apiUrl = inject(API_URL);
+
+  private urlApi = this.apiUrl + 'Estudio/';
+
+  listaPorEvolucion(evolucionId: number): Observable<ResponseApi> {
+    return this.http.get<ResponseApi>(`${this.urlApi}ListaPorEvolucion/${evolucionId}`);
+  }
+
+  crear(request: Estudio): Observable<ResponseApi> {
+    return this.http.post<ResponseApi>(`${this.urlApi}Crear`, request);
+  }
+
+  editar(request: Estudio): Observable<ResponseApi> {
+    return this.http.put<ResponseApi>(`${this.urlApi}Editar`, request);
+  }
 }
