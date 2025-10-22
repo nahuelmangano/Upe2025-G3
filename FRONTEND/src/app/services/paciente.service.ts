09d9c64ef8e4b024d47d6288069b71172abcaafd
Nahuel Mangano <mangano.nahuel@gmail.com>
2025-10-22 11:29:52 -0300

    agrego front service medico,obrasocial,pacienteobrasocial,paciente

--- FRONTEND/src/app/services/paciente.service.ts
+++ FRONTEND/src/app/services/paciente.service.ts
@@ -1,8 +1,29 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { Observable } from "rxjs";
+import { API_URL } from "../app.config"; // token de configuración
+import { ResponseApi } from "../interfaces/response-api";
+import { Paciente } from "../interfaces/paciente";
 
 @Injectable({
   providedIn: 'root'
 })
 export class PacienteService {
+    private http = inject(HttpClient);
+    private apiUrl = inject(API_URL);
+    private urlApi = this.apiUrl + 'Paciente/';
+
+    lista(): Observable<ResponseApi> {
+        return this.http.get<ResponseApi>(this.urlApi + 'Lista');
+    }
+    crear(request: Paciente): Observable<ResponseApi> {
+        return this.http.post<ResponseApi>(this.urlApi + 'Crear', request);
+    }
+    editar(request: Paciente): Observable<ResponseApi> {
+        return this.http.put<ResponseApi>(this.urlApi + 'Editar', request);
+    }
+    eliminar(id: number): Observable<ResponseApi> {
+        return this.http.delete<ResponseApi>(this.urlApi + 'Eliminar/' + id);
+    }
   
 }
