0a7d81cd1fdf0cdbdb756d265feb7c9792efede6
Manuel N. Mediavilla Grosjean <mediavilla.grosjean@gmail.com>
2025-11-03 12:52:52 -0300

    Actualizar layout component y routing module

--- FRONTEND/src/app/components/layout/layout-routing-module.ts
+++ FRONTEND/src/app/components/layout/layout-routing-module.ts
@@ -7,6 +7,10 @@ import { EvolucionesComponent } from '../../paciente/pages/evoluciones.component
 import { EvolucionFormComponent } from '../../paciente/pages/evolucion-form.component';
 import { PlantillaComponent } from '../plantilla/crear-plantilla/plantilla';
 import { ListaPlantillasComponent } from '../plantilla/lista-plantillas/lista-plantillas';
+import { ListaPacientesMedicoComponent } from '../../paciente/pages/lista-pacientes-medico/lista-pacientes-medico';
+import { ListaPacientesRecepcionistaComponent } from '../../paciente/pages/lista-pacientes-recepcionista/lista-pacientes-recepcionista';
+import { CrearPacienteFormComponent } from '../../paciente/pages/crear-paciente-form/crear-paciente-form'
+import { EditarPacienteFormComponent } from 'src/app/paciente/pages/editar-paciente-form/editar-paciente-form';
 import { LayoutComponent } from './layout.component';
 
 const routes: Routes = [
@@ -14,7 +18,11 @@ const routes: Routes = [
     path: '',
     component: LayoutComponent,
     children: [
-      { path: 'usuario', component: UsuarioComponent },
+      { path: 'usuarios', component: UsuarioComponent },
+      { path: 'recepcionista/pacientes/crear', component: CrearPacienteFormComponent },
+      { path: 'recepcionista/paciente/:id/editar', component: EditarPacienteFormComponent },
+      { path: 'recepcionista/pacientes', component: ListaPacientesRecepcionistaComponent },
+      { path: 'medico/pacientes', component: ListaPacientesMedicoComponent },
       { path: 'pacientes/:id/problemas', component: ProblemasComponent },
       { path: 'pacientes/:id/problemas/nuevo', component: ProblemaFormComponent },
       { path: 'pacientes/:id/evoluciones', component: EvolucionesComponent },
